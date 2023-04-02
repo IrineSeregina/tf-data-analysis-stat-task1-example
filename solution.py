274fb5a74c66566a82caa56a3d99b2e390488534
IrineSeregina <73775364+IrineSeregina@users.noreply.github.com>
2023-04-02 19:29:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,6 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    
+    new_array = np.log(x-11)
+    return new_array.mean() # Ваш ответ
